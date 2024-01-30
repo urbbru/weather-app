94ab550d7a0273528b76eee9c341c384655cd50e
tim kryha <tim@kryha.io>
2024-01-30 17:22:02 +0100

    fix file name

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 export * from "./app-shell";
-export * from "./cities-weather-details";
 export * from "./cities-weather-info";
 export * from "./city-searcher";
+export * from "./city-weather-details";
 export * from "./page-shell";
 export * from "./spinner";
